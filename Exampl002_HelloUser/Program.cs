80f800cdd47febd84487ee81096d6d478f5c7e10
ArturGert <arturgert89@yandex.ru>
2022-11-28 21:25:21 +0300

    add PretCoDe

--- Exampl002_HelloUser/Program.cs
+++ Exampl002_HelloUser/Program.cs
@@ -1,5 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите Ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Hi, ");
-Console.Write(username);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите Ваше имя ");
+        string username = Console.ReadLine();
+        Console.Write("Hi, ");
+        Console.Write(username);
+    }
+}
